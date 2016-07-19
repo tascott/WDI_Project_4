35f9079d36a11b38459b7a5bb5e0bbff9cce4ac4
Tom Scott <tom.a.scott@hotmail.co.uk>
2016-07-19 15:44:04 +0100

    de

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var app            = express();
 var config         = require('./config/config');
 var User           = require('./models/user');
 var secret         = require('./config/config').secret;
-// var sass           = require('node-sass');
+var sass           = require('node-sass');
 
 
 // mongoose.connect(config.database);
@@ -71,37 +71,37 @@ app.use("/api", routes);
 
 
 
-// var s3opt = new aws.S3({ /* ... */ })
+var s3opt = new aws.S3({ /* ... */ })
 
-// var upload = multer({
+var upload = multer({
 
-//   storage: s3({
-//     s3: s3opt,
-//     // the folder within the bucket
-//     dirname: 'uploads',
-//     // set this to your bucket name
-//     bucket: process.env.WDI_S3_BUCKET,
-//         // your AWS keys
-//     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-//     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-//     // the region of your bucket
-//     region: 'eu-west-1',
-//     // IMPORTANT: set the mime type to that of the file
-//     contentType: function(req, file, next) {
-//       next(null, file.mimetype);
-//     },
-//     // IMPORTANT: set the file's filename here
-//     // ALWAYS CHANGE THE FILENAME TO SOMETHING RANDOM AND UNIQUE
-//     // I'm using uuid (https://github.com/defunctzombie/node-uuid)
-//     filename: function(req, file, next) {
-//       // Get the file extension from the original filename
-//       var ext = '.' + file.originalname.split('.').splice(-1)[0];
-//       // create a random unique string and add the file extension
-//       var filename = uuid.v1() + ext;
-//       next(null, filename);
-//     }
-//   })
-// });
+  storage: s3({
+    s3: s3opt,
+    // the folder within the bucket
+    dirname: 'uploads',
+    // set this to your bucket name
+    bucket: process.env.WDI_S3_BUCKET,
+        // your AWS keys
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    // the region of your bucket
+    region: 'eu-west-1',
+    // IMPORTANT: set the mime type to that of the file
+    contentType: function(req, file, next) {
+      next(null, file.mimetype);
+    },
+    // IMPORTANT: set the file's filename here
+    // ALWAYS CHANGE THE FILENAME TO SOMETHING RANDOM AND UNIQUE
+    // I'm using uuid (https://github.com/defunctzombie/node-uuid)
+    filename: function(req, file, next) {
+      // Get the file extension from the original filename
+      var ext = '.' + file.originalname.split('.').splice(-1)[0];
+      // create a random unique string and add the file extension
+      var filename = uuid.v1() + ext;
+      next(null, filename);
+    }
+  })
+});
 
 
 var routes = require('./config/routes');
