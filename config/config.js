0eef866f9b4afd0f768156a7509312a8cd8e3046
Tom Scott <tom.a.scott@hotmail.co.uk>
2016-07-22 14:33:37 +0100

    url to uri

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   'port': process.env.PORT || 3000,
   'secret': process.env.JWT_SECRET || 'jsonwebtokensaregreat',
-  'database': process.env.MONGODB_URL || 'mongodb://localhost:27017/myMoments'
+  'database': process.env.MONGODB_URI || 'mongodb://localhost:27017/myMoments'
 };
