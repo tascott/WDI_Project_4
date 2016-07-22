cd2d860ddfdf3cb145b6d83baea5caa7a7b85c95
Tom Scott <tom.a.scott@hotmail.co.uk>
2016-07-22 14:53:19 +0100

    event route

--- public/js/controllers/eventsController.js
+++ public/js/controllers/eventsController.js
@@ -53,7 +53,7 @@ function EventsController(Event, $scope, $http, CurrentUser, $stateParams, $stat
    self.currentUser  = CurrentUser.getUser();
    self.event.user   = self.currentUser._id;
    console.log(self.event)
-   $http.post("http://localhost:3000/api/events/" , {event: self.event}, function(data) {
+   $http.post("/api/events/" , {event: self.event}, function(data) {
      console.log("success");
    });
    $state.go('user',  { id: CurrentUser.getUser()._id} );
